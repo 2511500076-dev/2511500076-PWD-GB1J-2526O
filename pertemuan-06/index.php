01786a0387f9772c23f2021b82c685bf88043323
Yohanes Setiawan Japriadi <ysetiawanj@atmaluhur.ac.id>
2025-10-30 18:06:05 +0700

    skrip php hello world

---
+++ pertemuan-06/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Judul Halaman</title>
+   <link rel="stylesheet" href="style.css">
+</head>
+
+   <body>
+    <header>
+        <h1>Ini Header</h1>
+        <button class="menu-toggle" id="menuToggle" aria-label="Toggle Navigation">
+            &#9776;
+        </button>
+        <nav>
+            <ul>
+                <li><a href="#home">Beranda</a></li>
+                <li><a href="#about">Tentang</a></li>
+                <li><a href="#contact">Kontak</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section id="home">
+            <h2>Selamat datang</h2>
+            <p>Ini contoh paragraf HTML.</p>
+            <?php
+    echo "Hallo Dunia!";
+     ?>
+        </section>
+        <section id="about">
+            <h2>Tentang Saya</h2>
+            <p><strong>NIM:</strong>2511500076 &#127891;</p>
+            <p><strong>Nama:</strong>Julio Putrawan &#128516;</p>
+            <P><strong>Tempat lahir:</strong>Pangkalpinang</P>
+            <p><strong>Tanggal lahir:</strong>01 Juli 2006 &#127874;</p>
+            <p><strong>Hobi:</strong>Bermain game &#127918;</p>
+            <p><strong>Pasangan:</strong>Belum ada &#128546;</p>
+            <p><strong>Pekerjaan:</strong>Teknisi &#128187;</p>
+            <p><strong>Nama Orang Tua:</strong>Ibu Yuliana &#129493;</p>
+            <p><strong>Nama Kakak:</strong>Tama Tamara &#128105;</p>
+            <p><strong>Nama adik:</strong>Barlian Tirta &#128102;</p>
+
+        </section>
+        <section id="contact">
+            <h2>Kontak kami</h2>
+            <form action="" method= "GET">
+                <label for="txtNama">
+                    <span>Nama:</span>
+                <input type="text" id="txtNama" name="txtNama" placeholder="Masukan Nama" required autocomplete="name">
+            </label>
+
+                <label for="txtEmail"><span>Email:</span>
+                <input type="email" id="txtEmail" name="txtEmail" placeholder="Masukan Email" required autocomplete="email">
+            </label>
+
+                <label for="txtPesan"><span>Pesan Anda:</span>
+                <textarea id="txtPesan" name="txtPesan" rows="4" placeholder="Tulis Pesan Anda..." required></textarea>
+            </label>
+
+                <button type="submit">Kirim</button>
+                <button type="reset">Batal</button>
+          
+            </form>
+
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2025 Julio Putrawan [2511500076]</p>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
