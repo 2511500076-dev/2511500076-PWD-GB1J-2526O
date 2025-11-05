416b4ce44aaf86957ce9a88b51f90e2ac4460ed5
Yohanes Setiawan Japriadi <ysetiawanj@atmaluhur.ac.id>
2025-11-05 22:33:48 +0700

    menambahkan label pada section ipk

--- pertemuan-06/README.md
+++ pertemuan-06/README.md
@@ -10,4 +10,5 @@ belajar sebagai berikut:
 <li>copy index.html, style.css pertemuan-05 ke 06</li>
 <li>menambahkan section about menggunakan echo</li>
 <li>menambahkan section ipk</li>
+<li>menambahkan label pada section ipk</li>
 </ol>
--- pertemuan-06/index.php
+++ pertemuan-06/index.php
@@ -119,7 +119,251 @@
              $mutu1 = "3.00";
              $bobot1 = "12";
              $status1 = "Lulus";
+             $namaMatkul2 = "Agama";
+             $sksMatkul2 = "2";
+             $nilaiHadir2 = "70";
+             $nilaiTugas2 = "50";
+             $nilaiUTS2 = "60";
+             $nilaiUAS2 = "80";
+             $nilaiAkhir2 = "67"; 
+             $grade2 = "B-";
+             $mutu2 = "2.70";
+             $bobot2 = "5.40";
+             $status2 = "Lulus";
+             $namaMatkul3 = "Matematika";
+             $sksMatkul3 = "4";
+             $nilaiHadir3 = "80";
+             $nilaiTugas3 = "70";
+             $nilaiUTS3 = "75";
+             $nilaiUAS3 = "80";
+             $nilaiAkhir3 = "76.50"; 
+             $grade3 = "B+";
+             $mutu3 = "3.30";
+             $bobot3 = "13.20";
+             $status3 = "Lulus";
+             $namaMatkul4 = "Fisika";
+             $sksMatkul4 = "7";
+             $nilaiHadir4 = "85";
+             $nilaiTugas4 = "75";
+             $nilaiUTS4 = "80";
+             $nilaiUAS4 = "85";
+             $nilaiAkhir4 = "81.50"; 
+             $grade4 = "A-";
+             $mutu4 = "3.70";
+             $bobot4 = "25.90";
+             $status4 = "Lulus";
+             $namaMatkul5 = "Pemrograman Web Dasar";
+             $sksMatkul5 = "3";
+             $nilaiHadir5 = "69";
+             $nilaiTugas5 = "80";
+             $nilaiUTS5 = "90";
+             $nilaiUAS5 = "100";
+             $nilaiAkhir5 = "89.90"; 
+             $grade5 = "E";
+             $mutu5 = "0.00";
+             $bobot5 = "0.00";
+             $status5 = "Gagal";
+
             ?>
+            <h2>Nilai Saya</h2>
+            <p>
+                <label>Nama Matakuliah ke-1:</label>
+                <?php 
+            echo $namaMatkul1
+            ?>
+            </p>
+            <p>
+                <label>SKS:</label>
+            <?php 
+            echo $sksMatkul1
+            ?>
+        </p>
+            <P>
+                <label>Kehadiran:</label>
+                <?php 
+                echo $nilaiHadir1
+                ?>
+            </P>
+            <p>
+                <label>Tugas:</label>
+                <?php 
+                echo $nilaiTugas1
+                ?>
+            </p>
+            <p><label>UTS:</label>
+            <?php
+            echo $nilaiUTS1
+             ?>
+        </p>
+            <p>
+                <label>UAS:</label>
+            <?php 
+            echo $nilaiUAS1
+            ?>
+        </p>
+            <p>
+                <label>Nilai Akhir:</label>
+                <?php 
+                echo $nilaiAkhir1
+                ?>
+        </p>
+            <p>
+                <label>Grade:</label>
+            <?php 
+            echo $grade1
+            ?>
+        </p>
+            <p>
+                <label>Angka Mutu:</label>
+            <?php 
+            echo $mutu1
+            ?>
+        </p>
+            <p>
+                <label>Bobot:</label>
+            <?php 
+            echo $bobot1
+            ?>
+        </p>
+            <p>
+                <label>Status:</label>
+            <?php 
+            echo $status1
+            ?>
+        </p>
+        <p>
+                <label>Nama Matakuliah ke-2:</label>
+                <?php 
+            echo $namaMatkul2
+            ?>
+            </p>
+            <p>
+                <label>SKS:</label>
+            <?php 
+            echo $sksMatkul2
+            ?>
+        </p>
+            <P>
+                <label>Kehadiran:</label>
+                <?php 
+                echo $nilaiHadir2
+                ?>
+            </P>
+            <p>
+                <label>Tugas:</label>
+                <?php 
+                echo $nilaiTugas2
+                ?>
+            </p>
+            <p><label>UTS:</label>
+            <?php
+            echo $nilaiUTS2
+             ?>
+        </p>
+            <p>
+                <label>UAS:</label>
+            <?php 
+            echo $nilaiUAS2
+            ?>
+        </p>
+            <p>
+                <label>Nilai Akhir:</label>
+                <?php 
+                echo $nilaiAkhir2
+                ?>
+        </p>
+            <p>
+                <label>Grade:</label>
+            <?php 
+            echo $grade2
+            ?>
+        </p>
+            <p>
+                <label>Angka Mutu:</label>
+            <?php 
+            echo $mutu2
+            ?>
+        </p>
+            <p>
+                <label>Bobot:</label>
+            <?php 
+            echo $bobot2
+            ?>
+        </p>
+            <p>
+                <label>Status:</label>
+            <?php 
+            echo $status2
+            ?>
+        </p>
+        <p>
+                <label>Nama Matakuliah ke-3:</label>
+                <?php 
+            echo $namaMatkul3
+            ?>
+            </p>
+            <p>
+                <label>SKS:</label>
+            <?php 
+            echo $sksMatkul3
+            ?>
+        </p>
+            <P>
+                <label>Kehadiran:</label>
+                <?php 
+                echo $nilaiHadir3
+                ?>
+            </P>
+            <p>
+                <label>Tugas:</label>
+                <?php 
+                echo $nilaiTugas3
+                ?>
+            </p>
+            <p><label>UTS:</label>
+            <?php
+            echo $nilaiUTS3
+             ?>
+        </p>
+            <p>
+                <label>UAS:</label>
+            <?php 
+            echo $nilaiUAS3
+            ?>
+        </p>
+            <p>
+                <label>Nilai Akhir:</label>
+                <?php 
+                echo $nilaiAkhir3
+                ?>
+        </p>
+            <p>
+                <label>Grade:</label>
+            <?php 
+            echo $grade3
+            ?>
+        </p>
+            <p>
+                <label>Angka Mutu:</label>
+            <?php 
+            echo $mutu3
+            ?>
+        </p>
+            <p>
+                <label>Bobot:</label>
+            <?php 
+            echo $bobot3
+            ?>
+        </p>
+            <p>
+                <label>Status:</label>
+            <?php 
+            echo $status3
+            ?>
+        </p>
+    
+            
+
         </section>
         <section id="contact">
             <h2>Kontak kami</h2>
@@ -142,9 +386,6 @@
           
             </form>
 
-        </section>
-        <section id="ipk">
-            <h2>Nilai Saya</h2>
         </section>
     </main>
     <footer>
