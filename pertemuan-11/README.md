2aa0382757f6e34e9f7e406e302c5f1443ece0eb
julio putrawan <2511500076@mahasiswa.atmaluhur.ac.id>
2025-12-11 16:59:54 +0700

    update file proses.php

---
+++ pertemuan-11/README.md
@@ -0,0 +1,13 @@
+# pertemuan-10
+
+silahkan sesuaikan dengan milik kalian masing-masing:<br><br>
+Hari ini, Rabu 11.Des.2025, saya:<br>
+NIM: 2511500076<br>
+Nama: Julio Putrawan<br>
+Kelompok: GB1J<br>
+<br>
+Belajar sebagai berikut:<br>
+<ol>
+<li>copy file pertemuan 10 ke 11</li>
+<li>update fungsi.php</li>
+</ol>
---
+++ pertemuan-11/localhost.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 04, 2025 at 11:12 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pwd2025`
+--
+CREATE DATABASE IF NOT EXISTS `db_pwd2025` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `db_pwd2025`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_tamu`
+--
+
+CREATE TABLE `tbl_tamu` (
+  `cid` int NOT NULL,
+  `cnama` varchar(100) DEFAULT NULL,
+  `cemail` varchar(100) DEFAULT NULL,
+  `cpesan` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_tamu`
+--
+
+INSERT INTO `tbl_tamu` (`cid`, `cnama`, `cemail`, `cpesan`) VALUES
+(1, 'Julio Putrawan', '2511500076@mahasiswa.atmaluhur.ac.id', 'Saya sangat senang belajar pemrograman web dasar'),
+(2, 'Ade Putra', 'adeputrapkp@gmail.com', 'saya sangat hebat pada pelajaran kalkulus'),
+(3, 'Maharani Indah Saputri', 'ranirani@gmail.com', 'saya kadang kurang teliti saat belajar pemrograman web dasar');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_tamu`
+--
+ALTER TABLE `tbl_tamu`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_tamu`
+--
+ALTER TABLE `tbl_tamu`
+  MODIFY `cid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
